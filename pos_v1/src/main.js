495235eaad9b9d224de260c4160297b592f27aa3
lyj <liyujing1990X@outlook.com>
2015-01-14 18:02:28 +0800

    get list items&details

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -2,42 +2,66 @@
 
 
 function printInventory(inputs) {
-  // var purchasedItems = getPurchasedItems(inputs);
-  // console.log(purchasedItems);
-  getPurchasedItems(inputs);
+  var purchasedItems = getPurchasedItems(inputs);
+  var list = getListItem(purchasedItems);
+//  var listItem = getListDetails (list);
+//  getListDetails (list);
+//  console.log(purchasedItems);
+  console.log(list);
 
 }
 
-function PurchasedItem(barcode, name, amount, price){
-  this.barcode = barcode;
-  this.name = name;
-  this.amount = amount;
-  this.price = price;
-}
+
 
 function isWeightedItem(input){
   return input.indexOf("-") != -1;
 }
 
-function getPurchasedItems(inputs){
+function getPurchasedItems(goods){
   var items = {};
   var barcode = "";
   var amount = 0;
-  for(var i=0; i<inputs.lenght; i++){
-    if (isWeightedItem(inputs[i])){
-      var r = inputs[i].split("-");
+  for(var i=0; i<goods.length; i++){
+    if (isWeightedItem(goods[i])){
+      var r = goods[i].split("-");
       barcode = r[0];
       amount = parseInt(r[1]);
       items[barcode] = amount;
       continue;
     }
-    barcode = inputs[i];
+    barcode = goods[i];
     if(items[barcode]) {
       items[barcode]++;
     }else {
       items[barcode] = 1;
     }
   }
-  console.log(items);
   return items;
+}
+
+function ListItem(barcode, amount, name, unit, price){
+  this.barcode = barcode;
+  this.amount = amount;
+  this.name = name;
+  this.unit = unit;
+  this.price = price;
+}
+
+function getListItem(items){
+  var allItems = loadAllItems();
+  var list = [];
+  for(var barcode in items){
+    listItem = new ListItem(barcode, items[barcode]);
+    list.push(listItem);
+  }
+  for (var i=0; i<list.length; i++){
+    for(var j=0; j<allItems.length; j++){
+      if (list[i].barcode == allItems[j].barcode){
+        list[i].name = allItems[j].name;
+        list[i].price = allItems[j].price;
+        list[i].unit = allItems[j].unit;
+      }
+    }
+  }
+  return list;
 }
